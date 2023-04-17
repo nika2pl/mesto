eb0bb9982370b48fafc67c65f8da8552bda9255b
Nikita Plokhikh <nika8123@ya.ru>
2023-04-17 19:47:22 +0300

    init

---
+++ vendor/js/index.js
@@ -0,0 +1,31 @@
+let profileName = document.querySelector('.profile__name');
+let profilePosition = document.querySelector('.profile__position')
+
+let nameInput = document.querySelector('.name_input');
+let jobInput = document.querySelector('.job_input');
+
+let formElement = document.querySelector('.popup__form')
+
+
+document.querySelector('.profile__edit-button').addEventListener('click', function() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profilePosition.textContent;
+
+    document.querySelector('#popup-edit-profile').classList.add('popup__opened');
+})
+
+
+document.querySelector('.popup__close-button').addEventListener('click', function() {
+    document.querySelector('#popup-edit-profile').classList.remove('popup__opened');
+})
+
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    
+    profileName.textContent = nameInput.value;
+    profilePosition.textContent = jobInput.value;
+    document.querySelector('#popup-edit-profile').classList.remove('popup__opened');
+}
+
+formElement.addEventListener('submit', handleFormSubmit); 
