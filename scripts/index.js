744c3e7dda244f38cf962dc1bdea81118674f88d
Nikita Plokhikh <nika8123@ya.ru>
2023-04-21 21:13:56 +0300

    second review

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,33 @@
 let profileName = document.querySelector('.profile__name');
-let profilePosition = document.querySelector('.profile__position')
+let profilePosition = document.querySelector('.profile__about')
 
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_position');
+let nameInput = document.querySelector('.popup__input_data_name');
+let jobInput = document.querySelector('.popup__input_data_position');
 
 let formElement = document.querySelector('.popup__form')
 
+let closeButton = document.querySelector('.popup__close-button');
+let editButton = document.querySelector('.profile__edit-button');
+let popupEditProfile = document.querySelector('#popup-edit-profile');
 
-document.querySelector('.profile__edit-button').addEventListener('click', function() {
+
+editButton.addEventListener('click', function () {
     nameInput.value = profileName.textContent;
     jobInput.value = profilePosition.textContent;
 
-    document.querySelector('#popup-edit-profile').classList.add('popup_opened');
+    popupEditProfile.classList.add('popup_opened');
 })
 
-
-document.querySelector('.popup__close-button').addEventListener('click', function() {
-    document.querySelector('#popup-edit-profile').classList.remove('popup_opened');
+closeButton.addEventListener('click', function () {
+    popupEditProfile.classList.remove('popup_opened');
 })
 
-
-function handleFormSubmit (evt) {
+function handleFormSubmit(evt) {
     evt.preventDefault();
-    
+
     profileName.textContent = nameInput.value;
     profilePosition.textContent = jobInput.value;
-    document.querySelector('#popup-edit-profile').classList.remove('popup_opened');
+    popupEditProfile.classList.remove('popup_opened');
 }
 
 formElement.addEventListener('submit', handleFormSubmit); 
