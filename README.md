d5ce971e3fd5e3b759876db67b2e821e365595bc
Nikita Plokhikh <nika8123@ya.ru>
2023-05-01 13:10:45 +0300

    Init 5-th sprint

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 **Figma**
 
-* [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+* [Спринт 4: макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+* [Спринт 5: макет в Figma](https://www.figma.com/file/bjyvbKKJN2naO0ucURl2Z0/JavaScript.-Sprint-5?node-id=0%3A1)
 
 
 # Сдача проекта Проект: Место
--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,61 @@ let profilePosition = document.querySelector('.profile__about')
 let nameInput = document.querySelector('.popup__input_data_name');
 let jobInput = document.querySelector('.popup__input_data_position');
 
-let formElement = document.querySelector('.popup__form')
+let placeInput = document.querySelector('.popup__input_data_place');
+let imageInput = document.querySelector('.popup__input_data_image');
 
-let closeButton = document.querySelector('.popup__close-button');
+let formElement = document.querySelector('.popup__form');
+let formAddCardElement = document.querySelector('.popup__add-card-form');
+
+let closeButton = document.querySelector('.popup__close-edit-profile-button');
+let closeButtonAddCard = document.querySelector('.popup__close-add-card-button');
+let closeButtonZoomPicture = document.querySelector('.popup__close-zoom-picture-button');
 let editButton = document.querySelector('.profile__edit-button');
-let popupEditProfile = document.querySelector('#popup-edit-profile');
+let addCardButton = document.querySelector('.profile__add-button');
 
+let popupEditProfile = document.querySelector('#popup-edit-profile');
+let popupAddCard = document.querySelector('#popup-add-card');
+
+let galleryList = document.querySelector('.gallery__list');
+
+let popupImage = document.querySelector('.popup__image');
+let popupDescription = document.querySelector('.popup__description');
+let popupZoomPicture = document.querySelector('.popup_zoom');
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+
+addCardButton.addEventListener('click', function () {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profilePosition.textContent;
+
+    popupAddCard.classList.add('popup_opened');
+})
 
 editButton.addEventListener('click', function () {
     nameInput.value = profileName.textContent;
@@ -18,8 +67,16 @@ editButton.addEventListener('click', function () {
     popupEditProfile.classList.add('popup_opened');
 })
 
+closeButtonAddCard.addEventListener('click', function () {
+    popupAddCard.classList.remove('popup_opened');
+})
+
 closeButton.addEventListener('click', function () {
     popupEditProfile.classList.remove('popup_opened');
+})
+
+closeButtonZoomPicture.addEventListener('click', function () {
+    popupZoomPicture.classList.remove('popup_opened');
 })
 
 function handleFormSubmit(evt) {
@@ -27,7 +84,56 @@ function handleFormSubmit(evt) {
 
     profileName.textContent = nameInput.value;
     profilePosition.textContent = jobInput.value;
+
     popupEditProfile.classList.remove('popup_opened');
 }
 
-formElement.addEventListener('submit', handleFormSubmit); 
+formElement.addEventListener('submit', handleFormSubmit);
+
+function handleFormAddCardSubmit(evt) {
+    evt.preventDefault();
+
+    galleryList.prepend(addCard(placeInput.value, imageInput.value))
+    popupAddCard.classList.remove('popup_opened');
+}
+
+formAddCardElement.addEventListener('submit', handleFormAddCardSubmit);
+
+let addCard = (name, link) => {
+    let contentCardTemplate = document.querySelector('#card-template').content;
+    let CardTemplate = contentCardTemplate.querySelector('.gallery__card').cloneNode(true);
+
+    let cardsImage = CardTemplate.querySelector('.gallery__image');
+    let cardName = CardTemplate.querySelector('.gallery__title');
+
+    cardName.textContent = name;
+    cardsImage.src = link;
+    cardsImage.alt = name;
+
+    // handle zoom picture
+    CardTemplate.addEventListener('click', function (evt) {
+        popupImage.src = cardsImage.src;
+        popupImage.alt = cardName.textContent;
+        popupDescription.textContent = cardName.textContent;
+
+        popupZoomPicture.classList.add('popup_opened');
+    });
+
+    // handle like button
+    CardTemplate.querySelector('.gallery__like-button').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('gallery__like_button_active');
+    });
+
+    // handle remove button
+    CardTemplate.querySelector('.gallery__delete-button').addEventListener('click', function (evt) {
+        evt.target.closest('.gallery__card').remove();
+    });
+
+    return CardTemplate;
+};
+
+initialCards.forEach(
+    card => {
+        galleryList.append(addCard(card.name, card.link))
+    }
+)
