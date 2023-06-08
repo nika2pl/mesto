6cc6f12fdc3943d535436344da6fb2a0d9bebe2b
Nikita Plokhikh <nika8123@ya.ru>
2023-06-08 17:41:22 +0300

    7 спринт: ооп - reupload

--- scripts/index.js
+++ scripts/index.js
@@ -2,40 +2,40 @@ import { Card } from "./Card.js";
 import { FormValidator } from "./FormValidator.js";
 
 const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
 ];
 
 const classListForm = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__input-error_active',
-  templateCard: '#card-template'
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__button',
+    inactiveButtonClass: 'popup__button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__input-error_active',
+    templateCard: '#card-template'
 };
 
 const profileName = document.querySelector('.profile__name');
@@ -65,43 +65,43 @@ const popupZoomPicture = document.querySelector('.popup_zoom');
 const closeButtons = document.querySelectorAll('.popup__close-button');
 
 const openPopup = function (popup) {
-  document.addEventListener('keydown', handleEscButton);
-  popup.classList.add('popup_opened');
+    document.addEventListener('keydown', handleEscButton);
+    popup.classList.add('popup_opened');
 }
 
 const closePopup = function (popup) {
-  popup.classList.remove('popup_opened');
+    popup.classList.remove('popup_opened');
 }
 
 initialCards.forEach(
-  card => {
-    const newCard = new Card({ name: card.name, image: card.link }, classListForm.templateCard);
-    galleryList.append(newCard.renderCard());
-  }
+    card => {
+        const newCard = new Card({ name: card.name, image: card.link }, classListForm.templateCard);
+        galleryList.append(newCard.renderCard());
+    }
 )
 
 closeButtons.forEach((button) => {
-  // находим 1 раз ближайший к крестику попап 
-  const popup = button.closest('.popup');
-  // устанавливаем обработчик закрытия на крестик
-  button.addEventListener('click', () => closePopup(popup));
+    // находим 1 раз ближайший к крестику попап 
+    const popup = button.closest('.popup');
+    // устанавливаем обработчик закрытия на крестик
+    button.addEventListener('click', () => closePopup(popup));
 });
 
 
 // handle click overlay
 const handleClickOverlay = (evt) => {
-  if (evt.target.classList.contains('popup_opened')) {
-    closePopup(evt.target);
-  };
+    if (evt.target.classList.contains('popup_opened')) {
+        closePopup(evt.target);
+    };
 };
 
 // handle esc button
 const handleEscButton = (evt) => {
-  if (evt.key === 'Escape') {
-    const popupOpened = document.querySelector('.popup_opened');
-    document.removeEventListener('keydown', handleEscButton);
-    closePopup(popupOpened);
-  };
+    if (evt.key === 'Escape') {
+        const popupOpened = document.querySelector('.popup_opened');
+        document.removeEventListener('keydown', handleEscButton);
+        closePopup(popupOpened);
+    };
 };
 
 const addCardValidate = new FormValidator(classListForm, popupAddCard);
@@ -113,39 +113,39 @@ editProfileValidate.enableValidationCheck();
 
 // handle add card button
 addCardButton.addEventListener('click', function () {
-  addCardValidate.toggleButtonState();
+    addCardValidate.toggleButtonState();
 
-  openPopup(popupAddCard);
+    openPopup(popupAddCard);
 })
 
 // handle edit profile button
 editButton.addEventListener('click', function () {
-  editProfileValidate.toggleButtonState();
+    editProfileValidate.toggleButtonState();
 
-  openPopup(popupEditProfile);
+    openPopup(popupEditProfile);
 })
 
 // handle form submit
 const handleProfileFormSubmit = function (evt) {
-  evt.preventDefault();
+    evt.preventDefault();
 
-  profileName.textContent = nameInput.value;
-  profilePosition.textContent = jobInput.value;
+    profileName.textContent = nameInput.value;
+    profilePosition.textContent = jobInput.value;
 
-  evt.target.reset()
+    evt.target.reset()
 
-  closePopup(popupEditProfile);
+    closePopup(popupEditProfile);
 }
 
 const handleAddCardFormSubmit = function (evt) {
-  evt.preventDefault();
+    evt.preventDefault();
 
-  const newCard = new Card({ name: placeInput.value, image: imageInput.value }, classListForm.templateCard);
-  galleryList.append(newCard.renderCard());
+    const newCard = new Card({ name: placeInput.value, image: imageInput.value }, classListForm.templateCard);
+    galleryList.append(newCard.renderCard());
 
-  evt.target.reset()
+    evt.target.reset()
 
-  closePopup(popupAddCard);
+    closePopup(popupAddCard);
 }
 
 formAddCardElement.addEventListener('submit', handleAddCardFormSubmit);
